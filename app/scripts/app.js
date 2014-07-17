e22ea324832b367117209b8dc6f49786dd27a6a7
weltenkreuzer <mail@nilsmueller.info>
2014-07-17 12:24:46 +0200

    Buchdetails und externe JSON-Daten

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,25 +9,6 @@
  * Main module of the application.
  */
 
-var booksDB = TAFFY([
-  {id:1, titel: 'Harry Potter und der Stein der Weisen', autor: [1], cover:'http://ecx.images-amazon.com/images/I/51ZU%2BCvkTyL.jpg', reihe: 1, nummer: 1},
-  {id:2, titel: 'Harry Potter und die Kammer des Schreckens', autor: [1], cover:'http://ecx.images-amazon.com/images/I/51dNrc6oESL.jpg', reihe: 1, nummer: 2},
-  {id:3, titel: 'Die Tribute von Panem. Tödliche Spiele', autor: [2], cover:'http://ecx.images-amazon.com/images/I/517wkVZfQ2L.jpg', reihe: 2, nummer: 1},
-  {id:4, titel: 'Die Tribute von Panem. Gefährliche Liebe', autor: [2], cover:'http://ecx.images-amazon.com/images/I/51N1FEWWwRL.jpg', reihe: 2, nummer: 2},
-  {id:5, titel: 'Die sonderbare Buchhandlung des Mr. Penumbra', autor: [3], cover:'http://ecx.images-amazon.com/images/I/51FbIY49hZL.jpg'}
-]);
-
-var autorDB = TAFFY([
-  {id:1, vorname: 'Joanne K.', nachname: 'Rowling'},
-  {id:2, vorname: 'Suzanne', nachname: 'Collins'},
-  {id:3, vorname: 'Robin', nachname: 'Sloan'}
-]);
-
-var reiheDB = TAFFY([
-  {id:1, titel: 'Harry Potter'},
-  {id:2, titel: 'Die Tribute von Panem'}
-]);
-
 
 angular
   .module('bookmanApp', [
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,15 +8,39 @@
  * Controller of the bookmanApp
  */
 angular.module('bookmanApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.books = booksDB().get();
-    $scope.reihen = reiheDB().get();
-    $scope.autoren = autorDB().get();
+  .controller('MainCtrl', function ($scope, $http) {
+
+
+	$http.get('data/books.json')
+	       .then(function(res){
+	          var booksDB = TAFFY(res.data);
+	          $scope.books = booksDB().order('autor').get();
+	        });
+
+	$http.get('data/author.json')
+	       .then(function(res){
+	          var autorDB = TAFFY(res.data);
+	          $scope.autoren = autorDB().get();
+	        });
+
+	$http.get('data/series.json')
+	       .then(function(res){
+	          var reiheDB = TAFFY(res.data);
+	          $scope.reihen = reiheDB().get();
+	        });
+
+    $scope.abook = false;
+    
+   
+
+    $scope.active = function(index) {
+    	$scope.abook = $scope.books[index];
+    };
   })
   .controller('AuthorCtrl', function ($scope, $routeParams) {
     $scope.books = booksDB({autor:{has:parseInt($routeParams.authorId)}}).get();
     $scope.reihen = reiheDB().get();
-    $scope.autoren = autorDB().get();
+    $scope.autoren = autorDB().get();    
   })
   .controller('AuthorListCtrl', function ($scope) {
     $scope.autoren = autorDB().order('nachname').get();
