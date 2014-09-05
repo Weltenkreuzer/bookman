5da783b225ed833dac10a734003ea7a845b8132a
weltenkreuzer <mail@nilsmueller.info>
2014-09-05 16:32:17 +0200

    Anzeigelogik in Directives verschoben

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,46 +8,106 @@
  * Controller of the bookmanApp
  */
 angular.module('bookmanApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .run(function($http, $rootScope){
+  
+  $http.get('data/books.json')
+         .then(function(res){
+            $rootScope.booksDB = TAFFY(res.data);
+          });
 
+  $http.get('data/author.json')
+         .then(function(res){
+            $rootScope.autorDB = TAFFY(res.data);
+          });
 
-	$http.get('data/books.json')
-	       .then(function(res){
-	          var booksDB = TAFFY(res.data);
-	          $scope.books = booksDB().order('autor').get();
-	        });
-
-	$http.get('data/author.json')
-	       .then(function(res){
-	          var autorDB = TAFFY(res.data);
-	          $scope.autoren = autorDB().get();
-	        });
-
-	$http.get('data/series.json')
-	       .then(function(res){
-	          var reiheDB = TAFFY(res.data);
-	          $scope.reihen = reiheDB().get();
-	        });
-
-    $scope.abook = false;
+  $http.get('data/series.json')
+         .then(function(res){
+            $rootScope.reiheDB = TAFFY(res.data);
+          });
     
+  })
+  .controller('MainCtrl', function ($scope, $rootScope) {
+
+  $scope.books = $rootScope.booksDB().order('autor').get();
+  $scope.autoren = $rootScope.autorDB().get();
+  $scope.reihen = $rootScope.reiheDB().get();
+ 
+  $scope.mainview = 'standardView';
+  $scope.abook = false;
+  $scope.filters = { };  
+  $scope.amenu = 2;
    
 
+  $scope.active = function(index) {
+    $scope.amenu = 1;
+  	$scope.abook = $scope.books[index];
+  };
+
+  $scope.filter = function(property, filt) {
+    $scope.filters = {};
+    $scope.filters[property] = filt;
+
+    $scope.filter_author = "";
+    $scope.filter_series = "";
+
+  };
+
+  })
+  .controller('AuthorCtrl', function ($scope, $rootScope, $routeParams) {
+
+    var id = parseInt($routeParams.authorId);
+
+    $scope.books = $rootScope.booksDB({autor:{has:id}}).get();
+    $scope.autoren = $rootScope.autorDB().get();
+    $scope.curAuthor = $rootScope.autorDB({id:id}).first();
+    $scope.reihen = $rootScope.reiheDB().get();
+    $scope.mainview = 'authorView';
+
     $scope.active = function(index) {
-    	$scope.abook = $scope.books[index];
+      $scope.amenu = 1;
+      $scope.abook = $scope.books[index];
+    };
+
+    $scope.filter = function(property, filt) {
+      $scope.filters = {};
+      $scope.filters[property] = filt;
+
+      $scope.filter_author = "";
+      $scope.filter_series = "";
     };
   })
-  .controller('AuthorCtrl', function ($scope, $routeParams) {
-    $scope.books = booksDB({autor:{has:parseInt($routeParams.authorId)}}).get();
-    $scope.reihen = reiheDB().get();
-    $scope.autoren = autorDB().get();    
+  .directive('filterSeries', function(){
+    return {
+      replace:'true',
+      restrict:'EC',
+      templateUrl:'/views/filterSeries.html'
+    };
   })
-  .controller('AuthorListCtrl', function ($scope) {
-    $scope.autoren = autorDB().order('nachname').get();
-    $scope.reihen = reiheDB().order('titel').get();
+  .directive('filterAuthor', function(){
+    return {
+      replace:'true',
+      restrict:'EC',
+      templateUrl:'/views/filterAuthor.html'
+    };
   })
-  .controller('ReiheCtrl', function ($scope, $routeParams) {
-    $scope.books = booksDB({reihe:parseInt($routeParams.reiheId)}).get();
-    $scope.reihen = reiheDB().get();
-    $scope.autoren = autorDB().get();
+  .directive('bookDetailSm', function(){
+    return {
+      replace:'true',
+      restrict:'EC',
+      templateUrl:'/views/bookDetailSm.html'
+    };
+  })
+  .directive('standardView', function(){
+    return {
+      replace:'true',
+      restrict:'EACM',
+      templateUrl:'/views/standardView.html'
+    };
+  })
+  .directive('authorView', function(){
+    return {
+      replace:'true',
+      restrict:'EACM',
+      templateUrl:'/views/authorView.html'
+    };
   });
